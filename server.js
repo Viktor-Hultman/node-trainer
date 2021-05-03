4dbf9e8863c271f459040eb83ae9470582ab0293
Viktor Hultman <viktor-hultman@hotmail.com>
2021-05-03 16:20:35 +0200

    Added static html files, some GET requests and a POST request that add a foodItem to a foodList

--- server.js
+++ server.js
@@ -1,16 +1,62 @@
 var express = require('express'),
   path = require('path'),
-  app = express();
-
+  app = express(),
+  bodyParser = require('body-parser');
 
 app.set('port', 3000);
 
+const foodList = [
+  {
+    id: 1,
+    name: 'Bacon',
+    taste: 'Delicious'
+  }
+];
+
+//https://github.com/expressjs/body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
 //https://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
+//https://medium.com/@onejohi/building-a-simple-rest-api-with-nodejs-and-express-da6273ed7ca9
+app.get('/random', (req, res) => {
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  res.json({'number': Math.floor(Math.random() * 1023)});
+});
 
-app.get('/hello', (req, res) => {
-  res.send('<h2>Hello from the server!</h2>')
+//http://expressjs.com/en/api.html#req
+app.get('/custom_random/:num', (req, res) => {
+  res.send({'number': Math.floor(Math.random() * req.params.num)});
+});
+
+//https://medium.com/@haybams/build-a-restful-api-with-node-js-and-express-js-d7e59c7a3dfb
+// Denna länk är för både get requestet och post requestet under
+app.get('/list', (req, res) => {
+  res.send({
+    'list': foodList
+  });
+});
+
+app.post('/post', (req, res) => {
+  const newFood = {
+    id: foodList.length + 1,
+    name: req.body.name,
+    taste: req.body.taste
+  };
+
+  //https://stackoverflow.com/a/44048398
+  let obj = foodList.find(x => x.name === newFood.name)
+  if(obj === undefined) {
+    foodList.push(newFood);
+    res.send('Success, added food to list')
+  } else {
+    res.send('Sorry, that food is alredy on the list')
+  }
+  console.log(obj)
+
 })
 
 
