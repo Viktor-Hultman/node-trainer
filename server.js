1710d1a927bccf1ffed0cc9a9888fe703e2173a9
Viktor Hultman <viktor-hultman@hotmail.com>
2021-05-05 11:44:31 +0200

    Renamed the 'urls' for the requests

--- server.js
+++ server.js
@@ -13,52 +13,6 @@ const foodList = [
   }
 ];
 
-//https://github.com/expressjs/body-parser
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-
-//https://expressjs.com/en/starter/static-files.html
-app.use(express.static(path.join(__dirname, 'public')));
-
-//https://medium.com/@onejohi/building-a-simple-rest-api-with-nodejs-and-express-da6273ed7ca9
-app.get('/random', (req, res) => {
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-  res.json({'number': Math.floor(Math.random() * 1023)});
-});
-
-//http://expressjs.com/en/api.html#req
-app.get('/custom_random/:num', (req, res) => {
-  res.send({'number': Math.floor(Math.random() * req.params.num)});
-});
-
-//https://medium.com/@haybams/build-a-restful-api-with-node-js-and-express-js-d7e59c7a3dfb
-// Denna länk är för både get requestet och post requestet under
-app.get('/list', (req, res) => {
-  res.send({
-    'list': foodList
-  });
-});
-
-app.post('/post', (req, res) => {
-  const newFood = {
-    id: foodList.length + 1,
-    name: req.body.name,
-    taste: req.body.taste
-  };
-
-  //https://stackoverflow.com/a/44048398
-  let obj = foodList.find(x => x.name === newFood.name)
-  if(obj === undefined) {
-    foodList.push(newFood);
-    res.send('Success, added food to list')
-  } else {
-    res.send('Sorry, that food is alredy on the list')
-  }
-  console.log(obj)
-
-})
-
 const vowelCounter = (string, res) => {
   let counter = 0;
   for(let i = 0; i < string.length; i++)  {
@@ -90,10 +44,56 @@ const vowelCounter = (string, res) => {
       counter ++;
     }
   }
-  return res.send("Number of vowels in your string: " + counter)
+  return res.send({'numner of vowels':  counter})
 }
 
-app.post('/vowelCounter', (req, res) => {
+//https://github.com/expressjs/body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+//https://expressjs.com/en/starter/static-files.html
+app.use(express.static(path.join(__dirname, 'public')));
+
+//https://medium.com/@onejohi/building-a-simple-rest-api-with-nodejs-and-express-da6273ed7ca9
+app.get('/random', (req, res) => {
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  res.json({'number': Math.floor(Math.random() * 1023)});
+});
+
+//http://expressjs.com/en/api.html#req
+app.get('/custom_random/:num', (req, res) => {
+  res.send({'number': Math.floor(Math.random() * req.params.num)});
+});
+
+//https://medium.com/@haybams/build-a-restful-api-with-node-js-and-express-js-d7e59c7a3dfb
+// Denna länk är för både get requestet och post requestet under
+app.get('/food_list', (req, res) => {
+  res.send({
+    'list': foodList
+  });
+});
+
+app.post('/add_food', (req, res) => {
+  const newFood = {
+    id: foodList.length + 1,
+    name: req.body.name,
+    taste: req.body.taste
+  };
+
+  //https://stackoverflow.com/a/44048398
+  let obj = foodList.find(x => x.name === newFood.name)
+  if(obj === undefined) {
+    foodList.push(newFood);
+    res.send('Success, added food to list')
+  } else {
+    res.send('Sorry, that food is alredy on the list')
+  }
+
+})
+
+
+app.post('/vowel_counter', (req, res) => {
   vowelCounter(req.body.string, res)
 })
 
